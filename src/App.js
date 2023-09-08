048421e4b83ffa4618075a77019e94b1dc6e45c8
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-09-08 15:28:58 -0500

    tasks 1 & 2 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {Link, Route, Routes} from 'react-router-dom';
+import Form from "./Components/Form";
 
 const App = () => {
   return (
     <>
+      <Link id="order-pizza" to='/'>Order</Link> {/*links to form*/}
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      
+      <Routes>
+        <Route path='/' element={<Form />} />
+      </Routes>
     </>
   );
 };
