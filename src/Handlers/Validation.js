fccf749ff53709acd3ca4a0978876e2335d1b2e8
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-09-18 14:34:01 -0500

    working on validation

--- src/Handlers/Validation.js
+++ src/Handlers/Validation.js
@@ -1,9 +1,39 @@
 import React from "react"
+import * as Yup from 'yup'
 
-export default function validate(props) {
+export default function validate(item) {
+    const schema = Yup.object().shape({
+        name: Yup
+            .string()
+            .required('Please enter your full name')
+            .min(2, 'name must be at least 2 characters'),
+        size: Yup
+            .string()
+            .required('Please select a size')
+            .oneOf(['sm', 'md', 'lg'], 'Please select a size'),
+        toppings: Yup
+            .object()
+            .required('Please select at least 1 topping')
+            .shape({
+                cheese: Yup
+                    .boolean()
+                    .oneOf([true, false]),
+                pepperoni: Yup
+                    .boolean()
+                    .oneOf([true, false]),
+                mushrooms: Yup
+                    .boolean()
+                    .oneOf([true, false]),
+                pineapple: Yup
+                    .boolean()
+                    .oneOf([true, false]),
+            })
+
+    })
+
     return (
         <p>
-            {props.item} error message goes here
+            {item} error message goes here
         </p>
     )
 }
